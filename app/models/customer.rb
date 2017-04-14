e64e99ec3779d52446160a68173c04c4e3cc6afd
Prelang Builder <builder@prelang.com>
2017-04-14 18:42:09 +0000

    Defined validation for attribute 'zip' on model 'Customer'

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,5 @@ class Customer < ActiveRecord::Base
   has_many :computers
   validates_formatting_of :email, using: :email
   validates_formatting_of :phone, using: :us_phone
+  validates_formatting_of :zip, using: :us_zip
 end
